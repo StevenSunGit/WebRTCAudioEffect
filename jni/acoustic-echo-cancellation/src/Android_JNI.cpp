4801a5ba2d3b51c72e922d85c3493021abe11693
sunyunfei <sunyunfei@xiaomi.com>
2020-12-17 13:15:32 +0800

    add aec code
    Signed-off-by: sunyunfei <sunyunfei@xiaomi.com>

--- jni/acoustic-echo-cancellation/src/Android_JNI.cpp
+++ jni/acoustic-echo-cancellation/src/Android_JNI.cpp
@@ -1,15 +1,7 @@
-#include <stdint.h>
-#include <time.h>
-#include <pthread.h>
-#include <android/log.h>
-#include <string.h>
-#include "audio_aec.h"
-
 #include "com_feifei_webrtcaudioeffect_jni_AudioEffect.h"
 
 extern "C" JNIEXPORT jlong JNICALL Java_com_feifei_webrtcaudioeffect_AudioEffect_AcousticEchoCancellationUtils_aecCreate(JNIEnv *env, jobject thiz, jint sampleRate) {
-    void* handle = audio_process_aec_create(sampleRate);
-    return (jlong)handle;
+    return 0;
 }
 
 extern "C" JNIEXPORT int JNICALL Java_com_feifei_webrtcaudioeffect_AudioEffect_AcousticEchoCancellationUtils_aecProcess(JNIEnv *env, jobject thiz) {
@@ -17,5 +9,5 @@ extern "C" JNIEXPORT int JNICALL Java_com_feifei_webrtcaudioeffect_AudioEffect_A
 }
 
 extern "C" JNIEXPORT void JNICALL Java_com_feifei_webrtcaudioeffect_AudioEffect_AcousticEchoCancellationUtils_aecFree(JNIEnv *env, jobject thiz, jlong aecID) {
-    audio_process_aec_free((void*)aecID);
+    ;
 }
