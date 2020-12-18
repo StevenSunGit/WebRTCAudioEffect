0b36b3bbb50349f5f2121072a9b13a17a7ad520c
sunyunfei <sunyunfei@xiaomi.com>
2020-12-18 12:20:25 +0800

    add channels
    Signed-off-by: sunyunfei <sunyunfei@xiaomi.com>

--- app/src/main/java/com/feifei/webrtcaudioeffect/AudioEffect/AudioEffectUtils.java
+++ app/src/main/java/com/feifei/webrtcaudioeffect/AudioEffect/AudioEffectUtils.java
@@ -1,7 +1,7 @@
 package com.feifei.webrtcaudioeffect.AudioEffect;
 
 public class AudioEffectUtils {
-    public static int get10msBufferSizeInByte(int frequency){
-        return 10 * frequency / 1000 * 2;
+    public static int get10msBufferSizeInByte(int frequency, int channels){
+        return 10 * frequency / 1000 * channels;
     }
 }
