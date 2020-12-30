90b8fb44ae5943c6a4c861689cae598d2b98bc73
sunyunfei <sunyunfei@xiaomi.com>
2020-12-30 19:50:16 +0800

    add nsx
    Signed-off-by: sunyunfei <sunyunfei@xiaomi.com>

---
+++ app/src/main/java/com/feifei/webrtcaudioeffect/AudioEffect/NoiseSuppressionXUtils.java
@@ -0,0 +1,21 @@
+package com.feifei.webrtcaudioeffect.AudioEffect;
+
+public class NoiseSuppressionXUtils {
+    static {
+        System.loadLibrary("noise-suppression-x");
+    }
+
+    public static int get10msBufferSizeInByte(int frequency){
+        return 10 * frequency / 1000 * 16 / 8;
+    }
+
+    public native long nsxCreate();
+
+    public native int nsxInit(long nsHandler, int frequency);
+
+    public native int nsxSetPolicy(long nsHandler, int mode);
+
+    public native int nsxProcess(long nsHandler, short[] inputData, int num_bands, short[] outputData);
+
+    public native int nsxFree(long nsHandler);
+}
---
+++ jni/noise-suppression-x/src/Android_JNI.cpp
@@ -0,0 +1,76 @@
+#include <jni.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "signal_processing_library.h"
+#include "noise_suppression_x.h"
+#include "noise_suppression.h"
+#include "gain_control.h"
+
+#ifndef nullptr
+#define nullptr 0
+#endif
+
+/* 日志输出 */
+#define LOG_TAG "jni_NoiseSuppressionNative"
+#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG,__VA_ARGS__)
+#define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG,__VA_ARGS__)
+
+extern "C" JNIEXPORT jlong JNICALL Java_com_feifei_webrtcaudioeffect_AudioEffect_NoiseSuppressionXUtils_nsxCreate(JNIEnv *env, jobject thiz) {
+    NsxHandle *nsHandler = NULL;
+    WebRtcNsx_Create(&nsHandler);
+    return (long)nsHandler;
+}
+
+extern "C" JNIEXPORT jint JNICALL Java_com_feifei_webrtcaudioeffect_AudioEffect_NoiseSuppressionXUtils_nsxInit(JNIEnv *env, jobject thiz, jlong nsHandler, jint frequency) {
+    NsxHandle *handler = (NsxHandle *) nsHandler;
+    if (handler == nullptr) {
+        return -3;
+    }
+    return WebRtcNsx_Init(handler, frequency);
+}
+
+extern "C" JNIEXPORT jint JNICALL Java_com_feifei_webrtcaudioeffect_AudioEffect_NoiseSuppressionXUtils_nsxSetPolicy(JNIEnv *env,jobject thiz, jlong nsHandler, jint mode) {
+    NsxHandle *handler = (NsxHandle *) nsHandler;
+    return WebRtcNsx_set_policy(handler, mode);
+}
+
+extern "C" JNIEXPORT jint JNICALL Java_com_feifei_webrtcaudioeffect_AudioEffect_NoiseSuppressionXUtils_nsxProcess(JNIEnv *env, jobject thiz, jlong nsHandler, jshortArray spframe, jint numBands, jshortArray outframe) {
+    NsxHandle *handler = (NsxHandle *) nsHandler;
+    jshort *cspframe = env->GetShortArrayElements(spframe, nullptr);
+    jshort *coutframe = env->GetShortArrayElements(outframe, nullptr);
+
+    int  filter_state1[6], filter_state12[6];
+    int  Synthesis_state1[6], Synthesis_state12[6];
+
+    memset(filter_state1,0,sizeof(filter_state1));
+    memset(filter_state12,0,sizeof(filter_state12));
+    memset(Synthesis_state1,0,sizeof(Synthesis_state1));
+    memset(Synthesis_state12,0,sizeof(Synthesis_state12));
+
+    short shInL[160],shInH[160];
+    short shOutL[160] = {0},shOutH[160] = {0};
+
+    //首先需要使用滤波函数将音频数据分高低频，以高频和低频的方式传入降噪函数内部
+    WebRtcSpl_AnalysisQMF(cspframe ,320, shInL, shInH,filter_state1,filter_state12);
+
+    //将需要降噪的数据以高频和低频传入对应接口，同时需要注意返回数据也是分高频和低频
+    int ret = WebRtcNsx_Process(handler ,shInL  ,shInH ,shOutL , shOutH);
+
+    //如果降噪成功，则根据降噪后高频和低频数据传入滤波接口，然后用将返回的数据写入文件
+    WebRtcSpl_SynthesisQMF(shOutL,shOutH,160,coutframe, Synthesis_state1,Synthesis_state12);
+
+    env->ReleaseShortArrayElements(spframe, cspframe, 0);
+    env->ReleaseShortArrayElements(outframe, coutframe, 0);
+    return ret;
+}
+
+extern "C" JNIEXPORT jint JNICALL Java_com_feifei_webrtcaudioeffect_AudioEffect_NoiseSuppressionXUtils_nsxFree(JNIEnv *env, jobject thiz, jlong nsHandler) {
+    NsxHandle *handler = (NsxHandle *) nsHandler;
+    WebRtcNsx_Free(handler);
+    return 0;
+}
